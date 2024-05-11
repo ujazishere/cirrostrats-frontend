671942fae62de9e8a01d8775de676c4eef9a0808
ismailsakhani <ismailsakhani879@gmail.com>
2024-05-12 04:41:21 +0530

    cleaned up home page and some design changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Input from "../components/Input";
 const Home = () => {
   return (
     <div className="home">
-      <h2 className="home__title">Cirrostrats </h2>
+      <h2 className="home__title">Check Weather, Gate And Flight Information.</h2>
 
       <Input />
 
@@ -17,12 +17,7 @@ const Home = () => {
       </NavLink>
 
       <div className="home__content">
-        <p>
-          !! Scheduled Tests Coming Up! Please save and access the main webapp through:
-          <span className="home__link"> cirrostrats.us.</span> Also, please save my number and feel free to reach out
-          for troubleshooting and general questions.
-          <span className="home__number"> +1 201-509-9485</span> - Ujas (UJ)
-        </p>
+
       </div>
     </div>
   );
