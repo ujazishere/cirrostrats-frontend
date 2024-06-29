76780fc201d3693cd9410bb6c28f046a37c16d4a
ujazishere <ujasvaghani@gmail.com>
2024-06-29 16:45:00 -0400

    Docker local progress w port
    Was able to expose the container's  port to the local port to access the server outside the container. Still working on the volumes integration.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // the host and port lines can allow vite to listen on all networks not jut on the same machine if running on a docker container.
     // Otherwise trying to talk from nginx or backend container to the frontend container can be a bitch. Same with package.json `dev` exposure
-  // host: '0.0.0.0',
-  // port: 5173,
-  plugins: [react()],
+  server:{
+    host: '0.0.0.0',
+    port: 5173,
+  },
+    plugins: [react()],
 })
