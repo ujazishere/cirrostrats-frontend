a8f8610fc3c68c7b1b712e0225810e2b588d308b
ujazishere <ujasvaghani@gmail.com>
2025-04-23 16:20:09 -0400

    formatSuggestions and fetchAndFilterSuggestions improvement
    Mainly cleaned up fetch and filter. this was responsible for getting suggestions data from the backend. Earlier it would have a while loop that would skim through page but that didnt seem practical and would increase complexity for little reward. Current version maintains pagination in the backend and each new keystroke fetch is requested from the backend.

--- src/components/Input/utils/formatSuggestions.js
+++ src/components/Input/utils/formatSuggestions.js
@@ -1,26 +1,27 @@
 import { getObjectType } from "./getObjectType";
 import { typeMap } from "./typeMap";
 
+// Gets triggered once on the initial render of the homepage. sets the initialSuggestions for the dropdown.
 export const formatSuggestions = (searchSuggestions) => {
   console.log('searchSuggestions', searchSuggestions);
   
   return Object.keys(searchSuggestions).map((eachItem) => ({
     id: searchSuggestions[eachItem]._id,
-    
+
     // Check if flightID exists, if it does, add the flightID property to the object
     ...(searchSuggestions[eachItem].flightID && {
     flightID: searchSuggestions[eachItem].flightID}),
-    
+
+    // label - this is what the user will see in the dropdown. It will be the flightID if it exists, otherwise it will be the airport code and name.
     label: searchSuggestions[eachItem].flightID
-        ? searchSuggestions[eachItem].flightID.startsWith('GJS')
-        // This is causing issues with submits.
+        ? searchSuggestions[eachItem].flightID.startsWith('GJS')    // if flightID starts with GJS, it's a UA flight. This is causing issues with submits.
         ? `UA${searchSuggestions[eachItem].flightID.slice(3)} (${searchSuggestions[eachItem].flightID})`
-        : searchSuggestions[eachItem].flightID
-        : `${searchSuggestions[eachItem].name} (${searchSuggestions[eachItem].code})`,
-    
-    // temp fix 'flight' if its undefined
-    type: typeMap[getObjectType(searchSuggestions[eachItem])] || 'flight',
-    
+        : searchSuggestions[eachItem].flightID                      // else  if it doesnt start with GJS, it other airline.
+        : `${searchSuggestions[eachItem].code} - ${searchSuggestions[eachItem].name}`,    // else if flightID doesnt exist, it's an airport.
+
+    // type - could be airport, flight, gate. - this is what /Details.jsx will use to request appropriate data from backend.
+    type: typeMap[getObjectType(searchSuggestions[eachItem])],
+
     // count: searchSuggestions[eachItem].count, // Optional
     // fuzzyFind: searchSuggestions[eachItem].fuzzyFind // Optional (if available)
 }))};
@@ -45,10 +46,7 @@ export const matchingSuggestion = (initialSuggestions, inputValue) => {
  * @param {Array} currentSuggestions - Existing suggestions to filter and base further searches on.
  * @param {string} inputValue - The input value used to filter the suggestions.
  * @param {string} userEmail - The user's email used for authentication or authorization in the search service.
- * @param {number} page - The current page number to start fetching from.
  * @param {Object} searchService - The service responsible for fetching search suggestions.
- * @param {number} [minRequiredResults=1] - The minimum number of required results to stop fetching.
- * @param {number} [maxPagesFetch=5] - The maximum number of pages to fetch before stopping.
  * @returns {Promise<Object>} - An object containing the filtered suggestions, the current page, and a boolean indicating if more pages are available.
  * @throws Will log an error message if fetching suggestions fails.
  */
@@ -57,61 +55,28 @@ export const fetchAndFilterSuggestions = async ({
   currentSuggestions,
   inputValue,
   userEmail,
-  page,
   searchService,
-  minRequiredResults = 1,
-  maxPagesFetch = 5,
-  
 }) => {
   // First filter the current suggestions
   let filteredSuggestions = matchingSuggestion(currentSuggestions, inputValue);
-  let currentPage = page;
-  let hasMorePages = true;      // declaring more pages in backend
-
-    //*****_____VVI____***** 
-  // ****Keep fetching more pages until it stacks more than 10 matches in filtered suggestions***
   // TODO: searching `aid` causes infinite while loop. it breaks after exhausting backend pages but keeps getting triggered.
-  while (filteredSuggestions.length < 1 && hasMorePages) {
-    // sleep(delayBetweenFetches);
-    try {
-      // Increment page for next fetch
-      // currentPage += 1; // First page has already been fetched during initial fetch.
+  try {
+    console.log('input, filteredSuggestions.length, ', inputValue, filteredSuggestions.length); // testing purposes only. remove later.
+    let rawSuggestions = await searchService.fetchMostSearched(
+      userEmail,
+      inputValue,
+    );
+    rawSuggestions = formatSuggestions(rawSuggestions);
       
-      // Fetch the next page of data
-      console.log('input, filteredSuggestions.length, currentPage', inputValue);
-      let rawSuggestions = await searchService.fetchMostSearched(
-        userEmail,
-        inputValue, //TODO: if hasMorePages is false(pages exhausted), serve actual input value to fetch outside of the mostPopularSearches.
-        currentPage, // page number for backend
-        10 // pageSize for backend
-      );
-      rawSuggestions = formatSuggestions(rawSuggestions);
-    //   clg.log('rawSuggestions', rawSuggestions.length);
-        
-      // If empty results(rawSuggestions) returned from backend, no more pages left
-      if (!rawSuggestions || rawSuggestions.length === 0) {
-        console.log('breaking, exhausted backend pages',rawSuggestions, rawSuggestions.length);
-        hasMorePages = false;
-        currentPage = 0;
-        break;
-      } {
-        console.log('rawSuggestions', rawSuggestions.length);}
+    filteredSuggestions = matchingSuggestion(rawSuggestions, inputValue);
+    
+    console.log('filtered suggestions', filteredSuggestions);
       
-      // Filter the new suggestions and add to existing results
-      const newFilteredSuggestions = matchingSuggestion(rawSuggestions, inputValue);
-      filteredSuggestions = [...filteredSuggestions, ...newFilteredSuggestions];
-      console.log('currentPage', currentPage);
-      console.log('filt', filteredSuggestions.length, filteredSuggestions);
-      
-    } catch (error) {
-      console.error("Error fetching suggestions:", error);
-      hasMorePages = false;
-      break
-    }
+  } catch (error) {
+    console.error("Error fetching suggestions:", error);
   }
+  
   return {
     newSuggestions:filteredSuggestions,
-    currentPage,
-    hasMorePages
   };
 };
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,6 +9,7 @@ import { highlightWeatherText } from "../components/utility/highlightWeatherText
  * @param {Object} props.weatherDetails - Weather data object
  */
 const WeatherCard = ({ arrow, title, weatherDetails }) => {
+  console.log('weatherDetails', weatherDetails);
   const datis = weatherDetails?.datis;
   const metar = weatherDetails?.metar;
   const taf = weatherDetails?.taf;
