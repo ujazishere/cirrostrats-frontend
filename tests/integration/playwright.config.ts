85567a8c115a2d5ddf916e31f31455de5a483d03
ujazishere <ujasvaghani@gmail.com>
2025-11-08 11:57:53 -0500

    github CI actions changes w tests

--- tests/integration/playwright.config.ts
+++ tests/integration/playwright.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
   expect: {
     timeout: 20000,
   },
-  retries: process.env.CI ? 2 : 5, // Will retry twice if CI (github actions), otherwise just once
-  workers: process.env.CI ? 1 : 3, // How many tests can run in parallel, 1 if (CI) github actions, 3 if locally
+  // remote : local
+  retries: process.env.CI ? 2 : 2, // How many retires on test fails- (CI) github actions vs(:) locally
+  workers: process.env.CI ? 5 : 5, // How many tests can run in parallel(workers)-  (CI) github actions, 3 if locally
   use: {
     baseURL: "http://localhost:5173/",
     headless: true, // For debugging, headless = false gives a GUI
